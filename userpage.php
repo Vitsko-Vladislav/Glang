387383958b3e15c218bc6dcc95dae1e9005249c0
Vitsko-Vladislav <Vitsko-Vladislav@yandex.ru>
2018-12-11 01:34:01 +0300

    Delete userpage.php

--- userpage.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Выбрать момент для распределения ролей
-session_start();
-$name = $_COOKIE['usrname'];
-echo $name;
-require_once('User.php');
-$user = new User(); // Добавить сборку конструктором
-
